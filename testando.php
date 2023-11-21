bebc03e8cdeeac180494bcccfd64bb6e592e293e
JHRK <jhrk05@gmail.com>
2023-11-20 22:37:45 -0300

    Deletando Arquivos desnecessários

--- testando.php
+++
@@ -1,9 +0,0 @@
-
-<?php 
-              $host="localhost";
-              $usuario="root";
-              $senha="";
-              $banco="banco_teste";
-          
-              $mysqli=new mysqli($host,$usuario,$senha,$banco);
-?>
