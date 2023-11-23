dcc300c99e59eaaf0e90ff034789bba37fa4fea0
JHRK <jhrk05@gmail.com>
2023-11-23 04:24:33 -0300

    Acesso ao acervo mostra tudo

--- Acervo.php
+++ Acervo.php
@@ -2,38 +2,39 @@
 include_once './header.php'
 ?>
 
+<?php 
+
+
+$host="localhost";
+$usuario="root";
+$senha="";
+$banco="Banco_Projeto";
+
+$mysqli=new mysqli($host,$usuario,$senha,$banco);
+
+
+if ($mysqli->connect_error) {
+    die("Erro na conexão: " . $mysqli->connect_error);
+}
+
+$Vomito="SELECT SRC from teste_sql";
+
+$Carregar=$mysqli->query($Vomito);
+
+
+while ($row = $Carregar->fetch_assoc()) {
+  echo ("<img src=".$row['SRC']." class='imagens'>");
+}
+
+
+
+$mysqli->close();
+
+?>
 
 
 
 
-      <main class="container-fluid col">
-              
-              <section class="col-lg-12 mt-3">
-                <div class="row" >
-                  <div class="col" ><a href="Acervo.php"><h2>Acervo</h2></a>
-                  <p>Imagens de diferentes espécies e estruturas observadas em microscópios.</p></div>
-                </div>
-              </section>
-              <section class="col-lg-12 mt-3">
-                <div class="row" >
-                  <div class="col" ><h2>Bactérias</h2>
-                  <p>Imagens de diferentes espécies e estruturas observadas em microscópios.</p></div>
-                </div>
-              </section>
-              <section class="col-lg-12 mt-3">
-                <div class="row" >
-                  <div class="col" ><h2>Tecidos</h2>
-                  <p>Imagens de diferentes espécies e estruturas observadas em microscópios.</p></div>
-                </div>
-              </section>
-              <section class="col-lg-12 mt-3">
-                <div class="row" >
-                  <div class="col" ><a href="Microscópios.php"><h2>Métodos</h2></a>
-                  <p>Entenda a diferença entre os métodos de observação.</p></div>
-                </div>
-              </section>
-
-      </main>  
 
       <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
