d34f2127d4fb90c972bb8f92b4b10c11cc5ed9df
JHRK <jhrk05@gmail.com>
2023-11-23 04:19:02 -0300

    Finalizações esteticas e ancoras

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
 
 
 
-<footer class="row justify-content-center mr-1" ><div class="col-2" style="border-left: 2px solid #4317f2; background-color:#ffffff;"><img src="./Img_Strg/logo-idr.png" class="col-8 offset-2 mt-3"><img src="./Img_Strg/LogoImageScience.jpg" class="col-8 offset-2"><img src="./Img_Strg/LogoCIL.jpg" class="col-8 offset-2"></div><img src="./Img_Strg/Subtract.png" class="col-3"></footer>
+<footer class="row justify-content-center mr-1" ><div class="col-2" style="border-left: 2px solid #4317f2; background-color:#ffffff;"><a href="https://idr.openmicroscopy.org"><img src="./Img_Strg/logo-idr.png" class="col-8 offset-2 mt-3"></a><a href="https://imagescience.org/images/"><img src="./Img_Strg/LogoImageScience.jpg" class="col-8 offset-2"></a><a href="http://cellimagelibrary.org/home"><img src="./Img_Strg/LogoCIL.jpg" class="col-8 offset-2"></a></div><img src="./Img_Strg/Subtract.png" class="col-3"></footer>
 
 
 </body>
