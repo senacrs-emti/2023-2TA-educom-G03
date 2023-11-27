9b84adb0c49c0d861350bd5f484f46438546ab9b
JHRK <jhrk05@gmail.com>
2023-11-27 01:49:08 -0300

    Adições no BD, ajustes na abertura de imagem

--- ImgAberta.php
+++ ImgAberta.php
@@ -1,18 +1,27 @@
 
 <?php
+include_once 'header.php';
 include './AcessoBanco.php';
 
 if(isset($_GET['id'])){
 
   $ImagemID = $_GET['id'];
-  $query = "SELECT SRC FROM teste_sql WHERE ID = $ImagemID";
+  $query = "SELECT * FROM teste_sql WHERE ID = $ImagemID";
   $chamar = mysqli_query($mysqli,$query);
 
   if ($chamar && $row = mysqli_fetch_assoc($chamar)) {
-    echo "<img src='" . $row['SRC'] . "' alt='Image'>";
+    echo "<img src='" . $row['SRC'] . "' alt='Image' class='imagem_aberta'>";
+    echo "<div class='container-fluid col mt-5'><h1>".$row['Nome']."</h1><p>".$row['Descri']."</p></div>";
   } 
 }else{
-  echo "Imagem não encontrada.";
+  echo "<img src='./Img_Strg/errorQuery.jpg'>";
+
 }
 
+?>
+
+
+
+<?php
+include_once 'footer.php';
 ?>
--- Microscópios.php
+++ Microscópios.php
@@ -15,9 +15,9 @@ include_once './header.php'
     Possui um limite de ampliação de até 1000 vezes,e é limitado para estruturas de até 0.2 micrômetros (0.000002 metros), o que permite uma visualização clara e detalhada de células.<br>
     É amplamente utilizado em análises clínicas por causa de sua praticidade.</p>
                 </div>
-                <img src="./Img_Strg/pawel-czerwinski-JdtUKqGdqw8-unsplash.jpg" style="height=25px; width:250px; margin-right:2.1%;  box-shadow: 5px 10px #4317f2;">
+                <img src="./Img_Strg/pawel-czerwinski-JdtUKqGdqw8-unsplash.jpg" class="imagens_métodos">
 </div>
-<div class="Acesso_Esquemas">Acesse o esquema<a href="./Img_Strg/ImgsBD/Group 20.png"><img src="./Img_Strg/ARROWIP.png" style="width:30px; height:15px; margin-left:10px;"></a></div>
+<div class="Acesso_Esquemas">Acesse o esquema<a href="./Img_Strg/ImgsBD/Optico.png"><img src="./Img_Strg/ARROWIP.png" style="width:30px; height:15px; margin-left:10px;"></a></div>
 </section>
 <section class="col-12 mt-3">
                 <div class="row" >
@@ -25,9 +25,9 @@ include_once './header.php'
     Devido a necessidade de analisar as amostras em um vácuo, não é possível observar espécimes vivos com este equipamento.<br>
     Possui um limite de observação de até 0.1 nanômetro (0.000000001 metros), sendo utilizado em análises de materiais ultra-finos e amostras orgânicas muito pequenas.</p> 
     </div>
-    <img src="./Img_Strg/michael-czGYgzmdYig-unsplash.jpg" style="height=25px; width:250px; margin-right:2.1%;  box-shadow: 5px 10px #4317f2;">
+    <img src="./Img_Strg/michael-czGYgzmdYig-unsplash.jpg" class="imagens_métodos">
     </div>
-    <div class="Acesso_Esquemas">Acesse o esquema<a href="./Img_Strg/ImgsBD/Group 18.png"><img src="./Img_Strg/ARROWIP.png" style="width:30px; height:15px; margin-left:10px;"></a></div>
+    <div class="Acesso_Esquemas">Acesse o esquema<a href="./Img_Strg/ImgsBD/Eletro.png"><img src="./Img_Strg/ARROWIP.png" style="width:30px; height:15px; margin-left:10px;"></a></div>
 </section>
 <section class="col-12 mt-3">
                 <div class="row" >
@@ -35,7 +35,7 @@ include_once './header.php'
     É ideal para análises topográficas, de propriedades materiais e de estruturas atômicas.<br>
     Por requerir alta precisão durante o procedimento, as amostras precisam ser planas, estáveis e condutivas. Além disso, não podem ocorrer distúrbios ou vibrações, senão a imagem final possuirá artefatos indesejados, portanto irregular. </p>
                 </div>
-                <img src="./Img_Strg/O-que-e-Microscopia-Eletronica-de-Varredura.jpg" style="height=25px; width:250px; margin-right:2.1%;  box-shadow: 5px 10px #4317f2;">
+                <img src="./Img_Strg/O-que-e-Microscopia-Eletronica-de-Varredura.jpg" class="imagens_métodos">
                 </div>
                 <div class="Acesso_Esquemas" >Acesse o esquema<a href="./Img_Strg/ImgsBD/Varredura.png"><img src="./Img_Strg/ARROWIP.png" style="width:30px; height:15px; margin-left:10px;"></a></div>
 </section>
